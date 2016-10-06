7f6e9a5941be96941eb6a3cfe49ee4db665bc96d
Rama Milaneh <ramamilaneh@Ramas-MacBook-Air.local>
2016-10-06 10:22:28 -0400

    Done

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,30 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    let animals = ["Dog","Cat","Mouse","Hamster","Bunny","Panda","Lion","Pig","Frog","Octopus"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        cell.textLabel?.text = animals[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AnimalDetailView" {
+            let destination = segue.destination as! AnimalViewController
+            destination.englishName = animals[(tableView.indexPathForSelectedRow?.row)!]
+        }
+    }
+    
+    
+    
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,47 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    
+    
+    @IBOutlet weak var englishNameLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var englishName: String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.englishNameLabel.text = englishName
+        self.emojiLabel.text = mappingNameToEmoji(name: englishName)
+//        self.navigationItem.title = englishName
+        
+        title = englishName
+        
+    }
+}
+
+ func mappingNameToEmoji(name: String) -> String {
+    switch name {
+    case "Dog" :
+        return "🐶"
+    case "Cat" :
+        return "🐱"
+    case "Mouse" :
+        return "🐭"
+    case "Hamster":
+        return "🐹"
+    case "Bunny":
+        return "🐰"
+    case "Panda":
+        return "🐼"
+    case "Lion":
+        return "🦁"
+    case "Pig":
+        return "🐷"
+    case "Frog":
+        return "🐸"
+    case "Octopus":
+        return "🐙"
+    default:
+        return ""
+    }
 }
